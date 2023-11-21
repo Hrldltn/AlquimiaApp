3ec19474fae83cd2206cf05103670e1a7ad4d198
hrldltn <haroldleytonol@gmail.com>
2023-11-21 02:38:05 -0300

    Merge changes from remote

--- Setup/settings.py
+++ Setup/settings.py
@@ -72,7 +72,7 @@ AUTH_USER_MODEL = 'AlquimiaApp.User'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'AlquimiaApp', 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'AlquimiaApp', 'Template')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
