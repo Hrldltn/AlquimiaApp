280cd0cccd95559b4cbfaac96d7bce9d5bddcbd3
hrldltn <haroldleytonol@gmail.com>
2023-12-04 03:14:51 -0300

    Actualizacion 1.5

--- Setup/settings.py
+++ Setup/settings.py
@@ -144,13 +144,9 @@ USE_I18N = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / 'static'
-STATICFILES_DIRS = [
-    BASE_DIR / 'AlquimiaApp' / 'static',
-]
 
-STATIC_URL = '/AlquimiaApp/static/'
-STATICFILES_DIRS = [BASE_DIR / 'AlquimiaApp' / 'static',]
+STATIC_URL = 'AlquimiaApp/static/'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
